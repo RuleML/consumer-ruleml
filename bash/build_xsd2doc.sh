e63d5803606ecab7b32fa6e2cf3395ebe7929929
greenTara <taraathan@gmail.com>
2015-02-16 09:49:10 -0500

    adding script for doc generation, but ignoring output (pre-release)

---
+++ bash/build_xsd2doc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Auto-generate Schema Docs from XSD
+# FIXME use configuration script to set path variables
+# FIXME use a configuration text file to loop over doc creation
+
+shopt -s nullglob
+BASH_HOME=$( cd "$(dirname "$0")" ; pwd -P )/
+REPO_HOME="${BASH_HOME}../"
+XSD_HOME=${REPO_HOME}xsd/
+OXY_HOME=/Applications/oxygen/
+DOC_SCRIPT=${OXY_HOME}schemaDocumentationMac.sh
+
+${DOC_SCRIPT}  ${XSD_HOME}consumer_normal.xsd  -cfg:${BASH_HOME}settings/doc.xml
+ 
